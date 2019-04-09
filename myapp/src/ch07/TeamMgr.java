e29648008fa82b28cacc299a6c105e2a5c9576e1
safesafe <49265806+safesafe@users.noreply.github.com>
2019-04-09 12:47:51 +0900

    Update TeamMgr.java

--- myapp/src/ch07/TeamMgr.java
+++ myapp/src/ch07/TeamMgr.java
@@ -1,9 +1,10 @@
 package ch07;
 
 import java.sql.*;
+import java.util.*;
 
 /*
- * DB¿Í °ü·ÃµÈ ±â´ÉÀÇ ¸Ş¼Òµå¸¦ ¼±¾ğÇÑ Å¬·¡½º
+ * DBì™€ ê´€ë ¨ëœ ê¸°ëŠ¥ì˜ ë©”ì†Œë“œë¥¼ ì„ ì–¸í•œ í´ë˜ìŠ¤
  */
 
 public class TeamMgr {
@@ -13,41 +14,135 @@ public class TeamMgr {
 	public TeamMgr() {
 		pool = DBConnectionMgr.getInstance();
 	}
-	//¸Ş¼Òµå ´Ù¸¸µé°í Mgr·Î ³Ñ¾î°¡´Â°ÍÀÌ Á¤¼®
+	// ë©”ì†Œë“œ ë‹¤ë§Œë“¤ê³  Mgrë¡œ ë„˜ì–´ê°€ëŠ”ê²ƒì´ ì •ì„
 	// Insert
 	public boolean insertTeam(TeamBean bean) {
 		Connection con = null;
 		PreparedStatement pstmt = null;
-		boolean flag = false; //flag´Â ¿ì¼±±ÇÀ» ÀÇ¹Ì
+		boolean flag = false; //flagëŠ” ìš°ì„ ê¶Œì„ ì˜ë¯¸
 		String sql = null;
 		try {
 			con = pool.getConnection();
 			sql = "insert tblTeam(name,city,age,team) values(?,?,?,?)";
 			pstmt = con.prepareStatement(sql);
-			//? ¼¼ÆÃ
+			//? ì„¸íŒ…
 			pstmt.setString(1, bean.getName());
 			pstmt.setString(2, bean.getCity());
 			pstmt.setInt(3, bean.getAge());
 			pstmt.setString(4, bean.getTeam());
-			int cnt = pstmt.executeUpdate(); // ½ÇÁ¦ ½ÇÇà
+			int cnt = pstmt.executeUpdate(); // ì‹¤ì œ ì‹¤í–‰
 			if(cnt == 1)
 				flag = true;
 		} catch (Exception e) {
 			e.printStackTrace();
 		}finally {
-			pool.freeConnection(con, pstmt); //connection ¹İ³³, pstmt close
+			pool.freeConnection(con, pstmt); //connection ë°˜ë‚©, pstmt close
 		}
 		return flag;
 	}
 	
-	
-	// Select
+	//List
+	public Vector<TeamBean> listTeam() {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String sql = null;
+		Vector<TeamBean> vlist = new Vector<TeamBean>();
+		try {
+			con = pool.getConnection();
+			sql = "select * from tblTeam";
+			pstmt = con.prepareStatement(sql);
+			rs = pstmt.executeQuery();		// ì‹¤í–‰
+			while(rs.next()) {
+				TeamBean bean = new TeamBean();
+				/* int num = rs.getInt("num");
+				bean.setNum(num); */
+				bean.setNum(rs.getInt("num"));
+				bean.setName(rs.getString("name"));
+				bean.setCity(rs.getString("city"));
+				bean.setAge(rs.getInt("age"));
+				bean.setTeam(rs.getString("team"));
+				vlist.addElement(bean);
+			}	// 
+		} catch (Exception e) {
+			e.printStackTrace();
+		}	finally {
+			pool.freeConnection(con, pstmt, rs);
+		}
+		return vlist;
+	}
 	
 	// Get
+	public TeamBean getTeam(int num) {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String sql = null;
+		
+		TeamBean bean = new TeamBean();
+		try {
+			con = pool.getConnection();
+			sql = "select num, name, city, age, team from tblTeam where num = ?";
+			pstmt = con.prepareStatement(sql);
+			pstmt.setInt(1, num);
+			rs = pstmt.executeQuery();
+			if(rs.next()) {
+				bean.setNum(rs.getInt(1));
+				bean.setName(rs.getString(2));
+				bean.setCity(rs.getString(3));
+				bean.setAge(rs.getInt(4));
+				bean.setTeam(rs.getString(5));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			pool.freeConnection(con, pstmt, rs);
+		}
+		return bean;
+	}
 	
 	// Update
+	public boolean updateTeam(TeamBean bean) {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		String sql = null;
+		boolean flag = false;
+		try {
+			con = pool.getConnection();
+			sql = "update tblTeam set name =?, city =?, age =?, team =? where num=?;";
+			pstmt = con.prepareStatement(sql);
+
+			pstmt.setString(1, bean.getName());
+			pstmt.setString(2, bean.getCity());
+			pstmt.setInt(3, bean.getAge());
+			pstmt.setString(4, bean.getTeam());
+			pstmt.setInt(5, bean.getNum());
+			if(pstmt.executeUpdate() == 1) {
+				flag = true;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			pool.freeConnection(con, pstmt);
+		}
+		return flag;
+	}
 	
 	// Delete
-	
+	public void deleteTeam(int num) {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		String sql = null;
+		try {
+			sql = "delete from tblTeam where num = ?";
+			pstmt 		= con.prepareStatement(sql);
+			pstmt.setInt(1, num);
+			pstmt.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			pool.freeConnection(con, pstmt);
+		}
+	}
 	
 }
